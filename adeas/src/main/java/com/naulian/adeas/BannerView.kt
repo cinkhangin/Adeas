a0a3ad76eaa3c18bb193831df1e9cebd87a1a9bf
Naulian <cinkhangin@gmail.com>
2023-02-24 11:51:57 +0630

    release 0.1.1

--- adeas/src/main/java/com/naulian/adeas/BannerView.kt
+++ adeas/src/main/java/com/naulian/adeas/BannerView.kt
@@ -2,15 +2,31 @@ package com.naulian.adeas
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.LayoutRes
 import com.google.android.material.card.MaterialCardView
 
-class BannerView(context: Context, attrs: AttributeSet) :
-    MaterialCardView(context, attrs) {
+class BannerView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : MaterialCardView(context, attrs, defStyleAttr) {
+
+    private val layoutInflater = LayoutInflater.from(context)
 
     init { loadAd() }
 
     private fun loadAd() {
+        if (isInEditMode) {
+            val preview = loadPreview(R.layout.banner_view)
+            addView(preview)
+            return
+        }
+
         Adeas.load(AdType.BANNER, context)
         addView(Adeas.adView)
+    }
+
+    private fun loadPreview(@LayoutRes layout: Int): View {
+        return layoutInflater.inflate(layout, this, true)
     }
 }
